91f3e7ec031e224dfaa1f3d4751d33a13e5bfa05
Omri Rajuan <omri93rajuan@gmail.com>
2025-11-08 22:30:15 +0200

    feat: enhance accessibility toolbar and dark mode styling

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -4,14 +4,18 @@ import { ErrorBoundary } from "../providers/ErrorBoundary";
 import { RouterProvider } from "../providers/RouterProvider";
 import { ThemeProvider } from "../providers/ThemeProvider";
 import { ToastContainer } from "react-toastify";
+import AccessibilityToolbar from "../components/accessibility/AccessibilityToolbar";
 import "react-toastify/dist/ReactToastify.css";
 export default function App() {
     return (<ThemeProvider>
       <I18nProvider>
         <QueryProvider>
           <ErrorBoundary>
-            <RouterProvider />
-            <ToastContainer position="top-right" rtl autoClose={1800} className="toast-container" toastClassName="toast-body"/>
+            <div id="a11y-app-shell" className="min-h-screen">
+              <RouterProvider />
+              <ToastContainer position="top-right" rtl autoClose={1800} className="toast-container" toastClassName="toast-body"/>
+            </div>
+            <AccessibilityToolbar />
           </ErrorBoundary>
         </QueryProvider>
       </I18nProvider>
