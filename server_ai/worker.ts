29c980b6e918f95261fa79a8524566c866696ef8
Omri Rajuan <omri93rajuan@gmail.com>
2026-08-30 16:05:35 +0300

    Add JWT access secret handling to worker configuration

--- server_ai/worker.ts
+++ server_ai/worker.ts
@@ -1,4 +1,11 @@
 import { httpServerHandler } from "cloudflare:node";
-import { server } from "./index";
+import { env } from "cloudflare:workers";
+
+const jwtAccessSecret = env.JWT_ACCESS_SECRET || process.env.JWT_ACCESS_SECRET;
+if (jwtAccessSecret) {
+  process.env.JWT_ACCESS_SECRET = jwtAccessSecret;
+}
+
+const { server } = await import("./index");
 
 export default httpServerHandler(server);
